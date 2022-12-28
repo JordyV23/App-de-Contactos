0ff6a48e468345c45c08e834eb7e0e493935e3be
JordyV23 <jordyvargas0123@gmail.com>
2022-12-27 18:31:23 -0600

    Contactos dinamicos

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
-
-$contacts = [
-  ["name" => "Pepe", "phone" => "88888888"],
-  ["name" => "Maria", "phone" => "89097883"],
-  ["name" => "Carlos", "phone" => "88061272"],
-  ["name" => "Marcos", "phone" => "83687541"],
-]
+if (file_exists("contacts.json")) {
+  $contacts = json_decode(file_get_contents("contacts.json"), true);
+} else {
+  $contacts = [];
+}
 
 ?>
 
@@ -41,10 +39,10 @@ $contacts = [
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="./index.html">Home</a>
+            <a class="nav-link" href="./index.php">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="./add.html">Add Contact</a>
+            <a class="nav-link" href="./add.php">Add Contact</a>
           </li>
         </ul>
       </div>
@@ -54,6 +52,14 @@ $contacts = [
   <main>
     <div class="container pt-4 p-3">
       <div class="row">
+        <?php if (count($contacts) == 0) : ?>
+          <div class="col-md-4 mx-auto">
+            <div class="card card-body text-center">
+              <p>No contacts saved yet</p>
+              <a href="add.php">Add One!</a>
+            </div>
+          </div>
+        <?php endif ?>
         <?php foreach ($contacts as $contact) { ?>
           <div class="col-md-4 mb-3">
             <div class="card text-center">
--- test.php
+++
@@ -1,5 +0,0 @@
-<?php $contacts = ["Pepe","Maria","Carlos"]; ?>
-
-<?php foreach($contacts as $contact){ ?>
-  <div><php $contact ?></div>
-<?php } ?>
